c8d8e412f87f790858a14098ec9dbf9c1063c65a
Naqi404 <112116609+Naqi404@users.noreply.github.com>
2026-02-18 16:22:15 -0500

    Delete main.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideEnvironmentNgxMask } from 'ngx-mask';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideEnvironmentNgxMask()
-  ]
-}).catch(err => console.error(err));
